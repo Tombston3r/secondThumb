6e92e2b8450bfc12e12330363107ea66069931ba
Tombston3r <mathieu.beoletto@esme.fr>
2026-04-10 10:15:11 +0200

    Refactor servo control code for improved functionality and readability

--- Code/FinalCode/FinalCode.ino
+++ Code/FinalCode/FinalCode.ino
@@ -1,12 +1,37 @@
+#include <Servo.h>
 
-int emgPin = A0; 
+Servo monServo;
+
+int emgPin = A7;
 int emgValue = 0;
 
+int angleMin = 40;
+int angleMax = 140;
+
 void setup() {
-Serial.begin(9600);
-};
+  Serial.begin(9600);
+  monServo.attach(9);
+}
+
 void loop() {
-emgValue = analogRead (emgPin); 
-Serial.println (emgValue);
-delay(50);
+  emgValue = analogRead(emgPin);
+  Serial.println(emgValue);
+
+  if (emgValue > 100) {
+    // Balayage de gauche à droite
+    Serial.println("Montée en cours")
+    for (int angle = angleMin; angle <= angleMax; angle++) {
+      monServo.write(angle);
+      delay(20);
+    }
+    Serial.println("Montée finie")
+  } else {
+    // Balayage de droite à gauche
+    Serial.println("Descente en cours")
+    for (int angle = angleMax; angle >= angleMin; angle--) {
+      monServo.write(angle);
+      delay(20);
+    }
+    Serial.prinln("Descente finie")
+  }
 }
--- Code/TestServo/TestServo.ino
+++ Code/TestServo/TestServo.ino
@@ -1,18 +1,29 @@
 #include <Servo.h>
-
-Servo myServo;
-int ServoPin = 9;
-
+ 
+Servo monServo;
+ 
+int angleMin = 40;   // limite basse pour CM509MG
+int angleMax = 140;  // limite haute pour CM509MG
+ 
 void setup() {
-  Serial.begin(9600);
-  myServo.attach(ServoPin);
+  monServo.attach(9);   // pin signal
 }
-
+ 
 void loop() {
-  myServo.write(90);
-  delay(50);
-  myServo.write(180);
-  delay(50);
-  myServo.write(0);
-  delay(50);
+  
+  // Balayage de gauche à droite
+  for (int angle = angleMin; angle <= angleMax; angle++) {
+    monServo.write(angle);
+    delay(20);
+  }
+ 
+  delay(1000);
+ 
+  // Balayage de droite à gauche
+  for (int angle = angleMax; angle >= angleMin; angle--) {
+    monServo.write(angle);
+    delay(20);
+  }
+ 
+  delay(1000);
 }
